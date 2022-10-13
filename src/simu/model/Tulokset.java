1e3543c5066e5bae8229ce687717011a99576eb1
rocksanen <rocksanen@gmail.com>
2022-10-13 12:12:23 +0300

    uusin,ehkä demoversio

--- src/simu/model/Tulokset.java
+++ src/simu/model/Tulokset.java
@@ -479,6 +479,7 @@ public class Tulokset implements Serializable{
 	 */
 	public double getKayttoaste_U(Palvelupisteet type) {		//Palauttaa palvelupisteen käyttöaste B/T
 	
+		
 		return (getAktiiviAika_B(type) / getKokonaisAika_T());
 		
 	}
@@ -500,7 +501,7 @@ public class Tulokset implements Serializable{
 	 */
 	public double getSuoritusteho_X(Palvelupisteet type) {		// palauttaa Throughput tai suoritusteho. C/T
 		
-		return (double) (getPalvelupisteenPalvelematAsiakkaat(type) / getKokonaisAika_T());
+		return (double) (getPalvelupisteenPalvelematAsiakkaat(type) / 210.0);
 		
 	}
 	
